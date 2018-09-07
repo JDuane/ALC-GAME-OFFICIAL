b254041a41ccfef42c20870b316287a84faf2f54
Jarett <jarett.r259@stu.nebo.edu>
2018-09-07 10:31:45 -0600

    character move

--- 2D Game/Assets/scrips/CharacterMove.cs
+++ 2D Game/Assets/scrips/CharacterMove.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class CharacterMove : MonoBehaviour {
 
-	public int moveSpeed = 10;
+	//Player Movement Variables
+	public int moveSpeed;
+	public float JumpHeight;
 
+	//Player ground variables
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public LayerMask whatIsGround;
+	private bool grounded;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
+
+	void FixedUpdate () {
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+	}
+
 	// Update is called once per frame
 	void Update () {
 		
+		// This code makes the character jump
+		if(Input.GetKeyDown (KeyCode.Space)&& grounded){
+			Jump();
+		}
+	}
+	
+	public void Jump(){
+		GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, JumpHeight);
 	}
 }
