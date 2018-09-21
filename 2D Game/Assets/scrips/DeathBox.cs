ccd2afc797c7c4394ce39e560621bfe9a19de49c
Jarett <jarett.r259@stu.nebo.edu>
2018-09-21 10:29:10 -0600

    score and death box

---
+++ 2D Game/Assets/scrips/DeathBox.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBox : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D other){
+
+		if(other.name == "pc")
+		{
+			Debug.Log("Player Enters Death Zone");
+			Destroy(other);
+		}
+	}
+}
---
+++ 2D Game/Assets/scrips/ScoreManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+
+	public static int Score;
+
+	Text ScoreText;
+
+	// Use this for initialization
+	void Start () {
+		ScoreText = GetComponent<Text>();
+
+		Score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Score < 0)
+			Score = 0;
+
+		ScoreText.text = " " + Score;
+	}
+}
