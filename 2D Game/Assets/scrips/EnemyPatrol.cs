06c74a8ef89a42977661a9047148805794c36c34
Jarett <jarett.r259@stu.nebo.edu>
2018-09-27 10:31:03 -0600

    made a enemy etc

---
+++ 2D Game/Assets/scrips/EnemyPatrol.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour {
+
+	//Move Variables
+	public float MoveSpeed;
+	public bool MoveRight;
+
+	//Wall Check
+	public Transform WallCheck;
+	public float WallCheckRadius;
+	public LayerMask WhatIsWall;
+	private bool HittingWall;
+
+	//Edge Check
+	private bool NotAtEdge;
+	public Transform EdgeCheck;
+
+	// Update is called once per frame
+	void Update () {
+		NotAtEdge = Physics2D.OverlapCircle(EdgeCheck.position, WallCheckRadius, WhatIsWall);
+
+		HittingWall = Physics2D.OverlapCircle(WallCheck.position, WallCheckRadius, WhatIsWall);
+		
+		if (HittingWall || !NotAtEdge){
+			MoveRight = !MoveRight;
+		}
+
+		if (MoveRight){
+			transform.localScale = new Vector3(-0.2f,0.2f,1f);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		else {
+			transform.localScale = new Vector3(0.2f,0.2f,1f);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+	}
+}
