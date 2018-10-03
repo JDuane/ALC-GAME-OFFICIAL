3b0afb32b50d72ed74dbb6abd77d68ae43cd5ea7
Jarett <jarett.r259@stu.nebo.edu>
2018-10-03 10:29:56 -0600

    A lot of things

---
+++ 2D Game/Assets/scrips/LevelManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public GameObject CurrentCheckPoint;
+	private Rigidbody2D pc;
+
+	// Particles
+	public GameObject DeathParticle;
+	public GameObject RespawnParticle;
+
+	// Respawn Delay
+	public float RespawnDelay;
+
+	// Point Penalty on Death
+	public int PointPenaltyOnDeath;
+
+	// Store Gravity Value
+	private float GravityStore;
+
+	// Use this for initialization
+	void Start () {
+		pc = FindObjectOfType<Rigidbody2D> ();
+	}
+	
+	public void RespawnPlayer(){
+		StartCoroutine ("RespawnpcCo");
+	}
+
+	public IEnumerator RespawnPlayerCo(){
+		// Generate Death Particle
+		Instantiate (DeathParticle, pc.transform.position, pc.transform.rotation);
+		// Hide pc
+		// pc.enabled = false
+		pc.GetComponent<Renderer>().enabled = false;
+		//Gravity Reset
+		GravityStore = pc.GetComponent<Rigidbody2D>().gravityScale;
+		pc.GetComponent<Rigidbody2D>().gravityScale = 0f;
+		pc.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+		// Point Penalty
+		ScoreManager.AddPoints(-PointPenaltyOnDeath);
+		// Debug Message
+		Debug.Log ("pc Respawn");
+		//Respawn Delay
+		yield return new WaitForSeconds (RespawnDelay);
+		// Gravity Restore
+		pc.GetComponent<Rigidbody2D>().gravityScale = GravityStore;
+		// Match pcs transform position
+		pc.transform.position = CurrentCheckPoint.transform.position;
+		// Show pc
+		// pc.enable = true;
+		pc.GetComponent<Renderer> ().enabled = true;
+		// Spawn pc
+		Instantiate (RespawnParticle, CurrentCheckPoint.transform.position, CurrentCheckPoint.transform.rotation);
+
+	}
+}
