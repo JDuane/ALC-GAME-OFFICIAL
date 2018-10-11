1794e0a5d278cadd14c6170ec1f6c1dd800cc9e4
Jarett <jarett.r259@stu.nebo.edu>
2018-10-11 10:30:20 -0600

    DeathEverywhere

---
+++ 2D Game/Assets/scrips/PlayerShoot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+
+	public Transform FirePoint;
+	public GameObject Projectile;
+
+	// Use this for initialization
+	void Start () {
+		Projectile = GameObject.Find("Projectile");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.RightControl))
+			Instantiate(Projectile,FirePoint.position, FirePoint.rotation);
+	}
+}
---
+++ 2D Game/Assets/scrips/Projectile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+	public float Speed;
+	public Rigidbody2D pc;
+	public GameObject EnemyDeath;
+	public GameObject ProjectileParticle;
+	public int PointsForKill;
+
+	// Use this for initialization
+	void Start () {
+		if(pc.transform.localScale.x < 0)
+			Speed = -Speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GetComponent<Rigidbody2D>().velocity = new Vector2(Speed, GetComponent<Rigidbody2D>().velocity.y);
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.tag == "Enemy"){
+			Instantiate(EnemyDeath, other.transform.position, other.transform.rotation);
+			Destroy (other.gameObject);
+			ScoreManager.AddPoints (PointsForKill);
+		}
+
+		Instantiate(ProjectileParticle, transform.position, transform.rotation);
+		Destroy (gameObject); 
+	}
+}
