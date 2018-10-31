eb059d82bbade6a768c65585a2e95ec14d9ca6ed
Jarett <jarett.r259@stu.nebo.edu>
2018-10-31 10:30:07 -0600

    did more stuff

---
+++ 2D Game/Assets/scrips/CameraFallow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraFallow : MonoBehaviour {
+
+	public CharacterMove Player;
+	public bool isFallowing;
+
+	// Camera position offset
+	public float xOffset;
+	public float yOffset;
+
+	// Use this for initialization
+	void Start () {
+		Player = FindObjectOfType<CharacterMove>();
+
+		isFallowing = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isFallowing){
+			transform.position = new Vector3(Player.transform.position.x + xOffset, Player.transform.position.y + yOffset, transform.position.z);
+		}
+	}
+}
