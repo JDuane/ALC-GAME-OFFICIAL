cdd752f373cc4807ecf1f3eecbf4273eb3d8fe88
Jarett <jarett.r259@stu.nebo.edu>
2018-12-03 10:09:31 -0700

    Did A lot of things

---
+++ 2D Game/Assets/scrips/MainMenu/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public int levelToLoad;
+
+	public void LoadLevel(){
+		SceneManager.LoadScene(levelToLoad);
+	}
+
+	public void LevelExit(){
+		Application.Quit();
+	}
+}
